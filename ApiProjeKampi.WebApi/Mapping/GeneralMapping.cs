3dfa01de84651c8efd9ad56c3aea014d5782de0e
Hakan Akinci <hakan062.2001@gmail.com>
2025-07-27 19:45:53 +0300

    Add Feature and Message DTOs, Controllers, and AutoMapper mappings

---
+++ ApiProjeKampi.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,26 @@
+﻿using ApiProjeKampi.WebApi.Dtos.FeatureDtos;
+using ApiProjeKampi.WebApi.Dtos.MessageDtos;
+using ApiProjeKampi.WebApi.Entities;
+using AutoMapper;
+
+namespace ApiProjeKampi.WebApi.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            // Feature mappings
+            CreateMap<Feature, ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature, CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature, UpdateFeatureDto>().ReverseMap();
+            CreateMap<Feature, GetByIdFeatureDto>().ReverseMap();
+
+            // Message mappings
+            CreateMap<Message, ResultMessageDto>().ReverseMap();
+            CreateMap<Message, CreateMessageDto>().ReverseMap();
+            CreateMap<Message, UpdateMessageDto>().ReverseMap();
+            CreateMap<Message, GetByIdMesaageDto>().ReverseMap();
+
+        }
+    }
+}
