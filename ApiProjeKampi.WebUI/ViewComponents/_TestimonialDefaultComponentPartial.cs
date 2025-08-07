3c29744edc5756a982653edc63d437190fe92276
Hakan Akinci <hakan062.2001@gmail.com>
2025-08-07 17:53:37 +0300

    Testimonial and Product add

---
+++ ApiProjeKampi.WebUI/ViewComponents/_TestimonialDefaultComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using ApiProjeKampi.WebUI.Dtos.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _TestimonialDefaultComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var clinet = _httpClientFactory.CreateClient();
+            var responseMessage = await clinet.GetAsync("https://localhost:7213/api/TestimonialS/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+    }
+}
