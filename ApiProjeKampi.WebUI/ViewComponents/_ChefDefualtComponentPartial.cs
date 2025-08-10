ac59ddee1e0855a61f113028ea608f5876f31e6e
Hakan Akinci <hakan062.2001@gmail.com>
2025-08-11 02:24:41 +0300

    Chef and contact add

---
+++ ApiProjeKampi.WebUI/ViewComponents/_ChefDefualtComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using ApiProjeKampi.WebUI.Dtos.ChefDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _ChefDefualtComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ChefDefualtComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var clinet = _httpClientFactory.CreateClient();
+            var responseMessage = await clinet.GetAsync("https://localhost:7213/api/Chefs/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultChefDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
